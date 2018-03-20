53c39eac80e8f152d4d332c8df0a9dce0287053b
Jirawat Ruenanukul <amplifiers9@gmail.com>
2018-03-21 05:21:16 +0700

    init project

---
+++ src/pages/home.vue
@@ -0,0 +1,13 @@
+<template>
+  <hello></hello>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld'
+
+export default {
+  components: {
+    'hello': HelloWorld
+  }
+}
+</script>
